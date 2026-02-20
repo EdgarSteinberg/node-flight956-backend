fb01896070c117b03463377f7fd6dee5ca9d864b
Edgar Steinberg <s.steinberg2019@gmail.com>
2026-02-20 11:59:50 -0300

    updated flight

--- src/utils/seedVuelos.js
+++ src/utils/seedVuelos.js
@@ -91,5 +91,6 @@ async function actualizarFechasVuelos() {
 
 actualizarFechasVuelos();
 
-// comando para correr la actualizacion de los vuelos node src/utils/seedVuelos.js
 // desde PS C:\Users\ACER\Documents\fligth956\node\node-flight956-backend>
+// comando para correr la actualizacion de los vuelos node src/utils/seedVuelos.js
+// no hace falta correr mas nada
